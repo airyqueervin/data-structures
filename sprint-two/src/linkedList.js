21a250611cc647f5308782b4bea857c225ed4f00
Hack Reactor Student <student@hackreactor.com>
2017-03-29 19:15:25 -0700

    Completed removeHead and addTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,21 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    var prevNode = this.tail;
+    this.tail = newNode;
+    prevNode.next = newNode;
   };
 
   list.removeHead = function() {
+    var newHead = this.head.next;
+    this.head = newHead;
   };
 
   list.contains = function(target) {
